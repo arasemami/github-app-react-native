88f37711f324881a00cf121d83bc5b44d8a6bebc
aras armani <aras.armani2014@gmail.com>
2018-07-23 02:11:24 +0300

    splash screen adde

---
+++ src/splash.js
@@ -0,0 +1,22 @@
+import React, {Component } from 'react';
+import { View , Text } from 'react-native';
+
+
+
+
+export default class Splash extends Component {
+
+render(){
+
+    return (
+
+     
+
+            <View>
+                    <Text>  Welcome to splash screen </Text>
+            </View>
+    
+    );
+}
+
+}
