c9bffbeba16b7c128a1420443eda232beef7218a
aras armani <aras.armani2014@gmail.com>
2018-07-24 10:40:40 +0300

    platform added

--- src/splash.js
+++ src/splash.js
@@ -1,5 +1,5 @@
 import React, {Component } from 'react';
-import { View , Text } from 'react-native';
+import { View , Text , StyleSheet , Platform } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
 
@@ -11,8 +11,9 @@ export default class Splash extends Component {
 
 
         componentDidMount() {
-                // do stuff while splash screen is shown
+            // do stuff while splash screen is shown
             // After having done stuff (such as async tasks) hide the splash screen
+            if (Platform.OS !== 'ios')
             SplashScreen.hide();
         }
 
@@ -24,18 +25,26 @@ render(){
 
     return (
 
-     
 
-            <View>
-                    <Text>  Welcome to splash screen </Text>
-                    <Text>  Welcome to splash screen </Text>
-                    <Text>  Welcome to splash screen </Text>
-                    <Text>  Welcome to splash screen </Text>
-                    <Text>  Welcome to splash screen </Text>
-                    <Text>  Welcome to splash screen </Text>
+
+            <View style={styles.container} >
+                    <Text >  wwww to splash screen </Text>
+                
             </View>
     
     );
 }
 
 }
+
+
+
+const styles = StyleSheet.create({
+        container:{
+                
+              flex:1,
+              textAlign:'center'
+
+        }
+
+})
