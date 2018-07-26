756ab15794b6141a9b369f74c5aa8f8d5384e3eb
aras armani <aras.armani2014@gmail.com>
2018-07-26 16:10:18 +0300

    addeing styles in index page

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import React , {Component} from 'react';
-import { View , Text , StyleSheet, Image } from 'react-native';
+import { View , Text , StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 
 
 
@@ -10,10 +10,22 @@ export default class Index extends Component {
 
         return (
                <View style={styles.container}>
-                <Image source={{uri: 'asset:/app_icon.png'}} style={{width: 40, height: 40}} />
-                   <Text>
-                       Hello to index page
+                <Image source={ require('../asset/img/logo.png')} style={{width:100, height:100, marginTop:100}} />
+                   <Text style={styles.githubAppText}>
+                      Github App
                    </Text>
+
+                   <View style={styles.textContainer}>
+                       <TextInput
+                       style={{height: 40 , width:400, paddingLeft:30}}
+                       placeholder="Seach github account . . ."
+                    //    onChangeText={(text) => this.setState({text})}
+                        />
+                   </View>
+
+                    <TouchableOpacity onPress={this._onPressButton} style={styles.searchButton}>
+                        <Text>Search</Text>
+                    </TouchableOpacity>
                </View>
         );
     }
@@ -25,13 +37,28 @@ const styles = StyleSheet.create({
 
 container:{
     flex:1,
-    backgroundColor:'#48C9B0',
+    backgroundColor:'#34495E',
     textAlign:'center',
     alignItems: 'center',
-    flexDirection: 'row',
-    
+    flexDirection: 'column',
+},
+githubAppText:{
+fontSize: 22,
+color:'#ECF0F1',
+fontWeight: 'bold'
 
-    
+},
+textContainer:{
+    width:400,
+    marginTop: 25,
+    backgroundColor:'#fff',
+    borderRadius: 5,
+    padding: 5
+},
+searchButton:{
+    backgroundColor:'#5DADE2',
+    width:400,
+    borderRadius: 5,
 
 
 }
